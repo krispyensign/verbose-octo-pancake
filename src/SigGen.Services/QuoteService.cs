50aeb71ccacab28cfe899ceab941d5fbc503ef45
krispyensign <futralj@gmail.com>
2025-03-14 22:23:50 -0400

    add historical and current account values

--- src/SigGen.Services/QuoteService.cs
+++ src/SigGen.Services/QuoteService.cs
@@ -7,4 +7,5 @@ public interface IQuoteService
 {
     Task<BigInteger> GetExactQuoteV4(BigInteger amountIn, string tokenSymbolIn, string tokenSymbolOut, string meta);
     Task<BigInteger> GetExactQuoteV2(BigInteger amountIn, string tokenSymbolIn, string tokenSymbolOut, string meta);
+    Task<Dictionary<string, BigInteger>> GetValueQuotes(Dictionary<string, BigInteger> balances, string startingToken, bool immutable);
 }
--- src/SigGen/Pages/Index.cshtml.cs
+++ src/SigGen/Pages/Index.cshtml.cs
@@ -1,16 +1,17 @@
 using System.Numerics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SigGen.Models;
 using SigGen.Services;
 
 namespace siggen.Pages;
 
-public class IndexModel(ILogger<IndexModel> logger, IQuoteService quoteService, IWalletService walletService) : PageModel
+public class IndexModel(ILogger<IndexModel> logger, IQuoteService quoteService, IWalletService walletService, QuoteConfiguration configuration) : PageModel
 {
     private readonly ILogger<IndexModel> _logger = logger;
     private readonly IQuoteService _quoteService = quoteService;
     private readonly IWalletService _walletService = walletService;
-
+    private readonly QuoteConfiguration _configration = configuration;
 
     // Bind the posted property so that the input value is available in OnPost.
     [BindProperty]
@@ -22,11 +23,18 @@ public class IndexModel(ILogger<IndexModel> logger, IQuoteService quoteService, 
 
     // API Call results
     public Dictionary<string, BigInteger> Balances { get; set;} = [];
+    public Dictionary<string, BigInteger> InitBalances { get; set;} = [];
+    public Dictionary<string, BigInteger> CurrentBalances { get; set; } = [];
     public string? QuoteResult1 { get; set; }
     public string? QuoteResult2 { get; set; }
+    public string? CurrentToken { get; set ; }
 
     public async Task<IActionResult> OnGet() {
         Balances = await _walletService.GetTokenBalances();
+        InitBalances = await _quoteService.GetValueQuotes(Balances, _configration.StartingToken, true);
+        CurrentToken ??= _configration.StartingToken;
+
+        CurrentBalances = await _quoteService.GetValueQuotes(Balances, CurrentToken, false);
 
         return Page();
     }
@@ -39,19 +47,25 @@ public class IndexModel(ILogger<IndexModel> logger, IQuoteService quoteService, 
             return Page();
         }
 
-        if (TokenInput == null|| TokenOutput == null)
+        if (TokenInput == null || TokenOutput == null)
         {
             _logger.LogError("TokenInput and TokenOutput cannot be null");
+
             return Page();
         }
 
         // Call the API service using the input from the textbox.
-        if (UInt128.TryParse(Amount, out var val))
+        if (BigInteger.TryParse(Amount, out var val))
         {
             var quoteResult1 = await _quoteService.GetExactQuoteV2(val, TokenInput, TokenOutput, "");
             QuoteResult1 = quoteResult1.ToString();
             var quoteResult2 = await _quoteService.GetExactQuoteV4(val, TokenInput, TokenOutput, "");
             QuoteResult2 = quoteResult2.ToString();
+        }
+        else
+        {
+            _logger.LogError("Amount must be of type BigInt");
+            return  Page();
         }
 
         Balances = await _walletService.GetTokenBalances();
