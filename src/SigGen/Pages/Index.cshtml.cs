0f26294177fb0e69de8009f53b2b2672c45d5073
krispyensign <futralj@gmail.com>
2025-03-15 22:48:58 -0400

    catch errors for some pools

--- src/SigGen/Pages/Index.cshtml.cs
+++ src/SigGen/Pages/Index.cshtml.cs
@@ -67,7 +67,7 @@ public class IndexModel(ILogger<IndexModel> logger, IQuoteService quoteService, 
         InitValues ??= await _quoteService.GetValueQuotes(Balances, TokenInput);
         foreach (var b in Balances)
         {
-            if (! InitValues.ContainsKey(b.Key))
+            if (!InitValues.ContainsKey(b.Key))
             {
                 InitValues.Add(b.Key, "0");
             }
